2da982aea6f317f53eb4c525eb4fc88ac252167f
root <root@imagex.sca.iu.edu>
2017-10-22 14:34:43 +0000

    Added user model and startup script

--- server/boot/user_setup.js
+++ server/boot/user_setup.js
@@ -1,12 +1,36 @@
 module.exports = function(app) {
-  var User = app.models.user;
+    var User = app.models.user;
+    var Role = app.models.Role;
+    var RoleMapping = app.models.RoleMapping;
 
-  User.create([
-    {username: 'Admin', email: 'youngmd@iu.edu', password: 'admin'},
-    {username: 'Guest', email: 'guest@imagex.sca', password: 'demo'}
-  ], function(err, users) {
-    if (err) console.log(err);
+    var lookupAndAddRole = function(user, rolename){
+        Role.findOrCreate({name: rolename}, function(err, role) {
+            role.principals.create({
+                principalType: RoleMapping.USER,
+                principalId: user.id
+            }, function (err, principal) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log('Created principal:', principal);
+                }
+            });
+        });
+    };
 
-    console.log('Created users:', users);
-  });
+    User.findOrCreate({ where: {email: 'youngmd@iu.edu'} }, {username: 'Admin', email: 'youngmd@iu.edu', password: 'admin'}, function(err, user) {
+        if (err){
+            console.log(err);
+        } else {
+            lookupAndAddRole(user,'admin');
+        }
+    });
+
+    User.findOrCreate({ where: {email: 'guest@imagex.sca'} }, {username: 'Guest', email: 'guest@imagex.sca', password: 'demo'}, function(err, user) {
+        if (err) {
+            console.log(err);
+        } else {
+            lookupAndAddRole(user, 'guest');
+        }
+    });
 }
